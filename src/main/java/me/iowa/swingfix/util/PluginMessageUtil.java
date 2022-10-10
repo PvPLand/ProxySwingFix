15126ad1e0a618032dca0b2d5d9ee8749d2cb9f9
Jaden <ilovefuud@gmail.com>
2022-10-10 02:37:50 -0500

    Fix badlion json object

--- src/main/java/me/iowa/swingfix/util/PluginMessageUtil.java
+++ src/main/java/me/iowa/swingfix/util/PluginMessageUtil.java
@@ -24,12 +24,13 @@ public class PluginMessageUtil {
         LUNAR_PACKET_BYTES = byteBuf.array();
         byteBuf.release();
 
-        JsonObject finalJson = new JsonObject(), animations = new JsonObject(), removeHitDelay = new JsonObject(), extraData = new JsonObject();
+        JsonObject finalJson = new JsonObject(),modsDisallowed = new JsonObject(), animations = new JsonObject(), removeHitDelay = new JsonObject(), extraData = new JsonObject();
         animations.addProperty("disabled", false);
         removeHitDelay.addProperty("forced", true);
         extraData.add("removeHitDelay", removeHitDelay);
         animations.add("extra_data", extraData);
-        finalJson.add("modsDisallowed", animations);
+        modsDisallowed.add("Animations", animations);
+        finalJson.add("modsDisallowed", modsDisallowed);
 
         BLC_PACKET_BYTES = finalJson.toString().getBytes(StandardCharsets.UTF_8);
     }
